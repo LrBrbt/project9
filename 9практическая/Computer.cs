49f0bda19aeba29d84ffd7d29876fb6aaa8676c0
LrBrbt <pogodinalerka@gmail.com>
2022-12-01 12:58:44 +0300

    Добавьте файлы проекта.

---
+++ 9практическая/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9практическая
+{
+    struct Computer
+    {
+        public string ProcessorType;
+        public int MemorySize;
+        public int HDDSize;
+        public string VideoCard;
+
+        public Computer(string processorType, int memorySize, int hardDiskDriveSize, string videoCard)
+        {
+            ProcessorType = processorType;
+            MemorySize = memorySize;
+            HDDSize = hardDiskDriveSize;
+            VideoCard = videoCard;
+        }
+    }
+}
